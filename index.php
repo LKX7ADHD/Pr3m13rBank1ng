014bcb44850d12876b00e783a83e748868c8dcf9
sebasyii <sebastian.yii.xh@gmail.com>
2020-11-06 17:59:02 +0800

    Created new sections and styled some sections.

--- index.php
+++ index.php
@@ -31,9 +31,9 @@ and open the template in the editor.
                             <h1>Get an extensive view of your cash flow.</h1>
                             <p>Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida.</p>
                         </div>
-                        <div class="banner__button">
-                            <a href="#">Get Started</a>
-                        </div>
+                        <button class="banner__button btn btn-success my-2 my-sm-0 py-3 px-4">
+                            Get Started
+                        </button>
                     </div>
                     <div class="col-lg-6">
                         <div class="banner__image">
@@ -52,39 +52,46 @@ and open the template in the editor.
     <main>
 
         <!-- Start of Grow Area -->
-        <section class="grow">
+        <section class="grow pt-5 pb-5">
             <div class="container">
                 <div class="row align-items-center">
                     <div class="col-lg-6">
                         <div class="grow__image">
-                            <img src="#" alt="#">
+                            <img src="assets/img/undraw_business_shop_qw5t.svg" alt="An SVG of a man pointing to a bank">
                         </div>
                     </div>
                     <div class="col-lg-6">
                         <div class="grow__content">
                             <span>Help your Business Grow</span>
-                            <h3>Created a business account for a fee-free & hassle-free</h3>
+                            <h3>Acquire New Business Opportunities Through Our Financial Solutions and Strategies.</h3>
                             <p>Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.</p>
                             <div class="grow__inner-content">
                                 <div class="number">
                                     <span>1</span>
                                 </div>
-                                <h4>Apply Anywhere</h4>
-                                <p>Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                                <div class="grow__container">
+                                    <h4>Open An Account In Minutes</h4>
+                                    <p>Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                                </div>
+                                
                             </div>
                             <div class="grow__inner-content">
                                 <div class="number">
                                     <span>2</span>
                                 </div>
-                                <h4>Apply Anywhere</h4>
-                                <p>Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                                <div class="grow__container">
+                                    <h4>0% Interest Rate</h4>
+                                    <p>Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                                </div>
                             </div>
                             <div class="grow__inner-content">
                                 <div class="number">
                                     <span>3</span>
                                 </div>
-                                <h4>Apply Anywhere</h4>
-                                <p>Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                                <div class="grow__container">
+                                    <h4>Financial Life at your fingertips</h4>
+                                    <p>Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -96,165 +103,98 @@ and open the template in the editor.
 
         <!--  -->
 
-        <section>
+        <section class="funding">
             <div class="container">
                 <div class="row align-items-center">
-                    <div class="col-lg-6">
-                        <div>
-                            <img src="#" alt="#">
-                        </div>
-                    </div>
+                    
                     <div class="col-lg-6">
                         <div class="content">
-                            <span>Help your Business Grow</span>
-                            <h3>Created a business account for a fee-free & hassle-free</h3>
-                            <p>Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.</p>
-                            <div class="">
-                                <div class="number">
-                                    <span>1</span>
-                                </div>
-                                <h4>Apply Anywhere</h4>
-                                <p>Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                            </div>
-                            <div class="">
-                                <div class="number">
-                                    <span>1</span>
-                                </div>
-                                <h4>Apply Anywhere</h4>
-                                <p>Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                            </div>
-                            <div class="">
-                                <div class="number">
-                                    <span>1</span>
-                                </div>
-                                <h4>Apply Anywhere</h4>
-                                <p>Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                            </div>
+                            <span>Reliable. Effective. Efficient</span>
+                            <h3>Fast Funding, like Tomorrow-fast.</h3>
+                            <p>No-hassle funding. Most businesses are approved the very next day.</p>
+
+                        </div>
+                        <button class="btn btn-primary btn-success my-5 py-3 px-4 btn__funding">
+                            Fund Your Business Now
+                        </button>
+                    </div>
+                    <div class="col-lg-6">
+                        <div>
+                            <img src="assets\img\undraw_investment_xv9d.svg" alt="#">
                         </div>
                     </div>
                 </div>
             </div>
         </section>
 
-        <section>
+<!-- Start Fun Facts Area -->
+        <section class="testimonial">
             <div class="container">
-                <div class="row align-items-center">
-                    <div class="col-lg-5">
-                        <div>
-                            <img src="#" alt="#">
-                        </div>
-                    </div>
-                    <div class="col-lg-7">
-                        <div class="content">
-                            <span>Help your Business Grow</span>
-                            <h3>Created a business account for a fee-free & hassle-free</h3>
-                            <p>Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.</p>
-                            <div class="">
-                                <div class="number">
-                                    <span>1</span>
-                                </div>
-                                <h4>Apply Anywhere</h4>
-                                <p>Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                            </div>
-                            <div class="">
-                                <div class="number">
-                                    <span>1</span>
-                                </div>
-                                <h4>Apply Anywhere</h4>
-                                <p>Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                            </div>
-                            <div class="">
-                                <div class="number">
-                                    <span>1</span>
-                                </div>
-                                <h4>Apply Anywhere</h4>
-                                <p>Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                            </div>
-                        </div>
-                    </div>
+                <div class="testimonial__container">
+                    <span>Why choose us</span>
+                    <h2>Our bank has been providing services to its customers for almost 25 years.</h2>
                 </div>
-            </div>
-        </section>
+                
+                <div class="row">
+                    <div class="col-lg-3 col-md-6 col-sm-6">
+                        <div class="single-fun-fact">
+                            <div class="icon">
+                                <i class="flaticon-positive-vote"></i>
+                            </div>
 
-        <section>
-            <div class="container">
-                <div class="row align-items-center">
-                    <div class="col-lg-6">
-                        <div>
-                            <img src="#" alt="#">
+                            <h3>
+                                <span class="odometer" data-count="358412">00</span>
+                            </h3>
+                            <p>Happy customers</p>
                         </div>
                     </div>
-                    <div class="col-lg-6">
-                        <div class="content">
-                            <span>Help your Business Grow</span>
-                            <h3>Created a business account for a fee-free & hassle-free</h3>
-                            <p>Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.</p>
-                            <div class="">
-                                <div class="number">
-                                    <span>1</span>
-                                </div>
-                                <h4>Apply Anywhere</h4>
-                                <p>Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                            </div>
-                            <div class="">
-                                <div class="number">
-                                    <span>1</span>
-                                </div>
-                                <h4>Apply Anywhere</h4>
-                                <p>Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                            </div>
-                            <div class="">
-                                <div class="number">
-                                    <span>1</span>
-                                </div>
-                                <h4>Apply Anywhere</h4>
-                                <p>Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
 
-        <section>
-            <div class="container">
-                <div class="row align-items-center">
-                    <div class="col-lg-5">
-                        <div>
-                            <img src="#" alt="#">
+                    <div class="col-lg-3 col-md-6 col-sm-6">
+                        <div class="single-fun-fact">
+                            <div class="icon">
+                                <i class="flaticon-confetti"></i>
+                            </div>
+
+                            <h3>
+                                <span class="odometer" data-count="25">00</span>
+                            </h3>
+                            <p>Years in banking</p>
                         </div>
                     </div>
-                    <div class="col-lg-7">
-                        <div class="content">
-                            <span>Help your Business Grow</span>
-                            <h3>Created a business account for a fee-free & hassle-free</h3>
-                            <p>Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.</p>
-                            <div class="">
-                                <div class="number">
-                                    <span>1</span>
-                                </div>
-                                <h4>Apply Anywhere</h4>
-                                <p>Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+
+                    <div class="col-lg-3 col-md-6 col-sm-6">
+                        <div class="single-fun-fact">
+                            <div class="icon">
+                                <i class="flaticon-bank"></i>
                             </div>
-                            <div class="">
-                                <div class="number">
-                                    <span>1</span>
-                                </div>
-                                <h4>Apply Anywhere</h4>
-                                <p>Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+
+                            <h3>
+                                <span class="odometer" data-count="2545">00</span>
+                            </h3>
+                            <p>Our branches</p>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 col-md-6 col-sm-6">
+                        <div class="single-fun-fact">
+                            <div class="icon">
+                                <i class="flaticon-success"></i>
                             </div>
-                            <div class="">
-                                <div class="number">
-                                    <span>1</span>
-                                </div>
-                                <h4>Apply Anywhere</h4>
-                                <p>Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                            </div>
+
+                            <h3>
+                                <span class="odometer" data-count="54285">00</span>
+                            </h3>
+                            <p>Successfully works</p>
                         </div>
                     </div>
                 </div>
             </div>
         </section>
+        <!-- End Fun Facts Area -->
+
+
+
+
     </main>
 
     <footer>
--- navbar.php
+++ navbar.php
@@ -1,9 +1,10 @@
     <!-- Nav -->
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">Navbar</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <a class="navbar-brand" href="#">Navbar</a>
+        
 
         <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
             <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
@@ -11,15 +12,15 @@
                     <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
+                    <a class="nav-link" href="#">Credit Card Loans</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                    <a class="nav-link" href="#" tabindex="-1" aria-disabled="true">Bank Loan</a>
                 </li>
             </ul>
             <form class="form-inline my-2 my-lg-0">
-                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+
+                <button class="btn btn-success my-2 my-sm-0 py-3 px-4" type="submit">Open Bank</button>
             </form>
         </div>
     </nav>
