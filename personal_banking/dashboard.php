4a75bc50a4060d9072e81825b8b5f29a6b7baf75
sebasyii <sebastian.yii.xh@gmail.com>
2020-11-14 21:51:01 +0800

    Dashboard Aside Bar

--- personal_banking/dashboard.php
+++ personal_banking/dashboard.php
@@ -17,29 +17,58 @@ To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <title></title>
     <?php include '../include/imports.inc.php' ?>
 </head>
+
 <body>
-<?php include '../include/navbar.inc.php' ?>
-<header class="jumbotron text-center">
-    <h1 class="display-4">My Accounts</h1>
-</header>
-<main class="container">
-    <ul class="list-group">
-        <?php
-        if (count($accounts) == 0 ) {
-            echo '<p class="lead">No Accounts</p>';
-        } else {
-            foreach ($accounts as $account) {
-                echo '<li class="list-group-item"><p class="h3">' . $account->getBalanceRepresentation() . '</p>' . $account->accountName . '<p class="text-muted mt-1 mb-0">' . $account->accountNumber . '</p></li>';
+    <?php include '../include/navbar.inc.php' ?>
+    <header class="jumbotron text-center">
+        <h1 class="display-4">My Accounts</h1>
+    </header>
+
+    <!-- Aside Bar -->
+    <aside>
+        <div class="row">
+            <div class="col-3">
+                <div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+                    <a class="nav-link active" id="v-pills-home-tab" data-toggle="pill" href="#v-pills-home" role="tab" aria-controls="v-pills-home" aria-selected="true">Home</a>
+                    <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false">Profile</a>
+                    <a class="nav-link" id="v-pills-messages-tab" data-toggle="pill" href="#v-pills-messages" role="tab" aria-controls="v-pills-messages" aria-selected="false">Messages</a>
+                    <a class="nav-link" id="v-pills-settings-tab" data-toggle="pill" href="#v-pills-settings" role="tab" aria-controls="v-pills-settings" aria-selected="false">Settings</a>
+                </div>
+            </div>
+            <div class="col-9">
+                <div class="tab-content" id="v-pills-tabContent">
+                    <div class="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab">...</div>
+                    <div class="tab-pane fade" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab">...</div>
+                    <div class="tab-pane fade" id="v-pills-messages" role="tabpanel" aria-labelledby="v-pills-messages-tab">...</div>
+                    <div class="tab-pane fade" id="v-pills-settings" role="tabpanel" aria-labelledby="v-pills-settings-tab">...</div>
+                </div>
+            </div>
+        </div>
+    </aside>
+    <!-- End of Aside Bar -->
+
+
+
+    <main class="container">
+        <ul class="list-group">
+            <?php
+            if (count($accounts) == 0) {
+                echo '<p class="lead">No Accounts</p>';
+            } else {
+                foreach ($accounts as $account) {
+                    echo '<li class="list-group-item"><p class="h3">' . $account->getBalanceRepresentation() . '</p>' . $account->accountName . '<p class="text-muted mt-1 mb-0">' . $account->accountNumber . '</p></li>';
+                }
             }
-        }
-        ?>
-    </ul>
-</main>
-<?php include '../include/footer.inc.php' ?>
+            ?>
+        </ul>
+    </main>
+    <?php include '../include/footer.inc.php' ?>
 </body>
+
 </html>
