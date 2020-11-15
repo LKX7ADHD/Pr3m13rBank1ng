1f949e2009b06314daae0698f5c826cf1f7c202b
Eddie <2002226@sit.singaporetech.edu.sg>
2020-11-16 02:58:55 +0800

    update sessiontimeout

--- include/sessiontimeout.inc.php
+++ include/sessiontimeout.inc.php
@@ -1,6 +1,6 @@
 <?php
 if (isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY'] > 60)) {
-		header('Location: ../logout.php');
+		header('Location: logout.php');
 		exit();
 }
 $_SESSION['LAST_ACTIVITY'] = time(); // update last activity time stamp
