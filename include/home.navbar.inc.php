2e8e45ed17263bbcb20127561e18a6ec798f1f52
Eddie <2002226@sit.singaporetech.edu.sg>
2020-12-03 12:18:57 +0800

    fix conflict

--- include/home.navbar.inc.php
+++ include/home.navbar.inc.php
@@ -1,11 +1,10 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light sticky-top">
-    <div class="container">
-        <a class="navbar-brand" href="/">Premier Banking</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbar"
-                aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-
+	<div class="container">
+		<a class="navbar-brand" href="/">Premier Banking</a>
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbar"
+		        aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
         <div class="collapse navbar-collapse pl-3 pl-lg-0" id="navbar">
             <ul class="navbar-nav mt-2 mt-lg-0">
                 <li class="nav-item">
