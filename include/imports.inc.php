fddbf34dfc2230d53d77a13a4e4e0816dd0f4305
LKX7_ADHD <29896527+LKX7ADHD@users.noreply.github.com>
2020-12-03 11:11:09 +0800

    theme-color to imports.inc.php (need add PWA manifest tho)

--- include/imports.inc.php
+++ include/imports.inc.php
@@ -1,7 +1,8 @@
+<meta name="theme-color" content="#5B73A8"/>
+
 <!-- fonts -->
 <link rel="preconnect" href="https://fonts.gstatic.com">
 <link href="https://fonts.googleapis.com/css2?family=Public+Sans&display=swap" rel="stylesheet">
-
 
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
       integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
