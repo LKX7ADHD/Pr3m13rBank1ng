dc326681b13c902aa44a978d4d89e9c184beb0ef
roy <sstroytay@gmail.com>
2020-12-03 12:59:07 +0800

    some stuff

--- include/home.navbar.inc.php
+++ include/home.navbar.inc.php
@@ -1,10 +1,10 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light sticky-top">
-	<div class="container">
-		<a class="navbar-brand" href="/">Premier Banking</a>
-		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbar"
-		        aria-expanded="false" aria-label="Toggle navigation">
-			<span class="navbar-toggler-icon"></span>
-		</button>
+    <div class="container">
+        <a class="navbar-brand" href="/">Premier Banking</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbar"
+                aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
         <div class="collapse navbar-collapse pl-3 pl-lg-0" id="navbar">
             <ul class="navbar-nav mt-2 mt-lg-0">
                 <li class="nav-item">
@@ -16,9 +16,7 @@
                     <a class="nav-link" href="/login.php">Login</a>
                 </li>
                 <li class="nav-item">
-                    <form action="register.php" method="post">
-                        <button class="btn btn__primary">Open Account</button>
-                    </form>
+                    <a class="btn btn__primary" href="/register.php">Open Account</a>
                 </li>
             </ul>
         </div>
