9acab20e0346dfb66ae7083e99ec351ee34083a0
roy <sstroytay@gmail.com>
2020-12-03 11:49:32 +0800

    some stuff

--- include/home.navbar.inc.php
+++ include/home.navbar.inc.php
@@ -1,27 +1,25 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light sticky-top">
-	<div class="container">
-		<a class="navbar-brand" href="/">Premier Banking</a>
-		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbar"
-		        aria-expanded="false" aria-label="Toggle navigation">
-			<span class="navbar-toggler-icon"></span>
-		</button>
+    <div class="container">
+        <a class="navbar-brand" href="/">Premier Banking</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbar"
+                aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
-		<div class="collapse navbar-collapse pl-3 pl-lg-0" id="navbar">
-			<ul class="navbar-nav mt-2 mt-lg-0">
-				<li class="nav-item">
-					<a class="nav-link" href="/about.php">About Us</a>
-				</li>
-			</ul>
-			<ul class="navbar-nav ml-auto">
-				<li class="nav-item">
-					<a class="nav-link" href="/login.php">Login</a>
-				</li>
-				<li class="nav-item">
-                    <form action="register.php" method="get">
-                        <button class="btn btn__primary">Open Account</button>
-                    </form>
-				</li>
-			</ul>
-		</div>
-	</div>  
+        <div class="collapse navbar-collapse pl-3 pl-lg-0" id="navbar">
+            <ul class="navbar-nav mt-2 mt-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link" href="/about.php">About Us</a>
+                </li>
+            </ul>
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="/login.php">Login</a>
+                </li>
+                <li class="nav-item">
+                    <a class="btn btn__primary" href="/register.php">Open Account</a>
+                </li>
+            </ul>
+        </div>
+    </div>
 </nav>
